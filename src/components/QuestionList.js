134feb46026eceed76816c6f11b57d8d07a8269b
Ian <ian.mosonik@student.moringaschool.com>
2024-11-13 15:32:36 +0300

    completed lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, [setQuestions]);
+
+  function handleDeleteQuestion(id) {
+    setQuestions(questions.filter((question) => question.id !== id));
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} onDelete={handleDeleteQuestion} />
+        ))}
+      </ul>
     </section>
   );
 }
